d899d57183eb65a2427822fee1d7f961c9689bc2
bosen L <bosenli2013@hotmail.com>
2022-03-27 09:23:30 -0400

    pokemon app created with react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+// import materialize third party library in Public folder index.html 
+
+
+// Requirement 1: Generate List of Pokemon from an API call
+// Modify the PokeList component so that it uses axios to make a request to the Pokéapi (https://pokeapi.co/api/v2/pokemon/) and then renders a "list" of Pokemon components where each Pokemon component contains data from the results from the API call.
+
+// For each pokemon, display it's name inside of a url.
+
+// Requirement 2: Toggle Pokemon Details
+// Modify the Pokemon component so that when clicked it displays detailed information about that Pokemon. If the Pokemon is clicked again, then the detailed information will be hidden (so the click should serve as a toggle that displays a pokemon's details if the details are currently hidden or hides the pokemon's details if the details are currently displayed).
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
